a7c41065cf369a1eee35bc7d1abfa1a958616408
Fiveolioo <96richardohc@gmail.com>
2020-12-01 21:22:13 -1000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  const win = arr.find(game => game.result === "W")
+  return win ? win.year : undefined
+}
